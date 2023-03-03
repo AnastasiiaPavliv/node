84155c58a7237ea4da948b03cc9cb37e3ae0b780
AnastasiiaPavliv <pavlivnastia@gmail.com>
2023-03-03 14:47:39 +0200

    hw-1

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ fs.mkdir(path.join('lesson1'),(err)=>{
     if (err) throw Error();
 })
 const worker = async ()=>{
+    try{
     const fileNames=['ls1.txt', 'ls2.txt','ls3.txt','ls4.txt','ls5.txt'];
     const folderNames=['L1','L2','L3','L4','L5',];
 
@@ -14,15 +15,25 @@ const worker = async ()=>{
     for (const fileName of fileNames){
         await fs.writeFile(path.join('lesson1', fileName), 'Hello');
     }
-    fs.stat(path.join('lesson1'),(err, stats)=>{
-        if (err) throw Error(err.message);
-        console.log(stats.isDirectory());
-    })
+        const files = await fs.readdir(path.join('lesson1'));
+        for (const file of files) {
+      const stats = await fs.stat(path.join('lesson1', file));
+      const isFile = stats.isFile();
+
+    if (isFile) {
+        console.log('Thi is file:', path.join('lesson1', file));
+    }else {
+        console.log('Thi is folder:', path.join('lesson1', file));
+    }}
 
     const data = await fs.readdir(path.join('lesson1'), {withFileTypes: true});
     data.forEach(file => {
         console.log(file.isFile());
     });
+
+    }catch (e){
+        console.log(e.message);
+    }
 
 }
 
